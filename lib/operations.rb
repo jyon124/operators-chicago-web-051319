21042c66515a916f6a7f12e940e8121edacbbed8
Jooyon Won <jyon1204@gmail.com>
2019-04-29 19:44:24 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,9 +1,9 @@
 def unsafe?(speed)
- if speed > 60
+ if (speed > 60)
 	 return "true";
- elsif speed < 40
+ elsif (speed < 40)
 	 return "true";
- elsif speed > 40 && speed < 60
+ elsif (speed > 40 && speed < 60)
 	 return "false"
 end
 
