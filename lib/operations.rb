efa71f4f50e79bebf61ab7817be5f8b1c492d4ca
Jooyon Won <jyon1204@gmail.com>
2019-04-29 19:41:10 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,6 +1,7 @@
 def unsafe?(speed)
-	return puts true if speed == unsafe;
-	return puts false if speed == safe;
+	return puts true if speed > 60;
+	return puts true if speed < 40;
+	return puts false if speed > 40 && speed < 60;
 end
 
 
